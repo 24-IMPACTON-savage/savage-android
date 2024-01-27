33c071c0e61e7b1920a13f14eb529537128a9d24
정승훈 <hks026naver@gmail.com>
2024-01-28 04:49:26 +0900

    feat :: membership screen

--- app/src/main/java/team/retum/savage_android/feature/root/NavigationGraph.kt
+++ app/src/main/java/team/retum/savage_android/feature/root/NavigationGraph.kt
@@ -1,10 +1,7 @@
 package team.retum.savage_android.feature.root
 
-import android.util.Log
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -13,6 +10,7 @@ import team.retum.savage_android.feature.complete.CompleteScreen
 import team.retum.savage_android.feature.findworker.FindWorkerScreen
 import team.retum.savage_android.feature.loading.LoadingScreen
 import team.retum.savage_android.feature.matchingworker.MatchingWorkerScreen
+import team.retum.savage_android.feature.membership.MembershipScreen
 import team.retum.savage_android.feature.onboarding.StartScreen
 import team.retum.savage_android.feature.onboarding.join.Join1Screen
 import team.retum.savage_android.feature.onboarding.join.Join2Screen
@@ -22,7 +20,6 @@ import team.retum.savage_android.feature.post.PostScreen
 import team.retum.savage_android.feature.postpay.PostPayScreen
 import team.retum.savage_android.feature.posttime.PostTimeScreen
 import team.retum.savage_android.feature.setting.SettingScreen
-import team.retum.savage_android.util.Constant.TAG
 
 
 @Composable
@@ -47,7 +44,7 @@ fun NavigationGraph(
             Join2Screen(navController = navController, name = name ?: "")
         }
         composable(NavGroup.Main.Map.id) {
-            FindWorkerScreen()
+            FindWorkerScreen(navController = navController)
         }
         composable(NavGroup.Onboarding.Login2.id + "/{name}") {
             val name = it.arguments?.getString("name")
@@ -57,10 +54,10 @@ fun NavigationGraph(
             Login1Screen(navController = navController)
         }
         composable(NavGroup.Main.Loading.id) {
-            LoadingScreen()
+            LoadingScreen(modifier = Modifier)
         }
         composable(NavGroup.Main.Complete.id) {
-            CompleteScreen()
+            CompleteScreen(modifier = Modifier)
         }
         composable(NavGroup.Main.Post.id) {
             PostScreen(navController = navController)
@@ -78,7 +75,10 @@ fun NavigationGraph(
             SettingScreen(navController = navController)
         }
         composable(NavGroup.Main.Map.id) {
-            FindWorkerScreen()
+            FindWorkerScreen(navController = navController)
+        }
+        composable(NavGroup.Main.Membership.id) {
+            MembershipScreen()
         }
     }
 }
