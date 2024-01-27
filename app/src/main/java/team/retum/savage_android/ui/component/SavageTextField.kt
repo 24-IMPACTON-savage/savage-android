997f55576db0c37e536bb0037ef4399bfa3310f5
Tmdhoon2 <hks026naver@gmail.com>
2024-01-28 03:12:35 +0900

    feat :: set address

--- app/src/main/java/team/retum/savage_android/ui/component/SavageTextField.kt
+++ app/src/main/java/team/retum/savage_android/ui/component/SavageTextField.kt
@@ -3,15 +3,16 @@ package team.retum.savage_android.ui.component
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material3.Divider
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -21,6 +22,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -97,11 +99,49 @@ fun SavageTextField(
                     ), text = hint, color = SavageColor.Gray40, style = SavageTypography.Body3
             )
         }
-        if (isHaveBottomLine)
-            Divider(
-                modifier = Modifier.fillMaxWidth(),
-                color = color,
+        if (isHaveBottomLine) Divider(
+            modifier = Modifier.fillMaxWidth(),
+            color = color,
+        )
+    }
+}
+
+@Composable
+internal fun SavageBoxTextField(
+    modifier: Modifier = Modifier,
+    value: String,
+    onValueChange: (String) -> Unit,
+    hint: String,
+) {
+    var focused by remember { mutableStateOf(false) }
+    val alpha by animateFloatAsState(
+        targetValue = if (value.isEmpty()) 1f
+        else 0f,
+        label = "",
+    )
+    BasicTextField(
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(4.dp))
+            .background(SavageColor.Gray10)
+            .padding(
+                horizontal = 16.dp,
+                vertical = 10.dp,
             )
+            .onFocusChanged {
+                focused = it.hasFocus
+            },
+        value = value,
+        onValueChange = onValueChange,
+    ) { innerTextField ->
+        innerTextField()
+        Text(
+            modifier = Modifier.alpha(alpha),
+            text = hint,
+            style = SavageTypography.Body2,
+            color = SavageColor.Gray40,
+        )
+
     }
 }
 
