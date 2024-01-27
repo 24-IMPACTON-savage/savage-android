48ce11e724c5d13ec8c71945a19b3a5c2bc2153f
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-01-27 16:00:21 +0900

    navigation

--- app/src/main/java/team/retum/savage_android/MainActivity.kt
+++ app/src/main/java/team/retum/savage_android/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.softpie.karabiner.ui.root.NavigationGraph
 import team.retum.savage_android.ui.component.SavageButton
 import team.retum.savage_android.ui.theme.SavageandroidTheme
 
@@ -13,8 +15,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SavageandroidTheme {
 
+            val navController = rememberNavController()
+
+            SavageandroidTheme {
+                NavigationGraph(navController = navController)
             }
         }
     }
---
+++ app/src/main/java/team/retum/savage_android/feature/root/NavigationGraph.kt
@@ -0,0 +1,28 @@
+package com.softpie.karabiner.ui.root
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import team.retum.savage_android.feature.root.NavGroup
+
+
+@Composable
+fun NavigationGraph(
+    navController: NavHostController,
+) {
+    NavHost(
+        navController = navController,
+        startDestination = getStartDestination()
+    ) {
+        composable(NavGroup.Onboarding.Start.id) {
+            Text(text = "start")
+        }
+    }
+}
+
+private val Start = NavGroup.Onboarding.Start
+
+fun getStartDestination() =
+    /*if (enableAutoLogin) NavGroup.Main.MAIN.id else */Start.id
