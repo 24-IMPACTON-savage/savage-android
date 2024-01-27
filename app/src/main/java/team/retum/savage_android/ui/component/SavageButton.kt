1a055fc48d25393ac76a2ded532374724a33514d
wsi1212 <wsi1212@dgsw.hs.kr>
2024-01-27 16:23:57 +0900

    feat :: Button component
    1/27

--- app/src/main/java/team/retum/savage_android/ui/component/SavageButton.kt
+++ app/src/main/java/team/retum/savage_android/ui/component/SavageButton.kt
@@ -1,29 +1,70 @@
 package team.retum.savage_android.ui.component
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import team.retum.savage_android.ui.theme.SavageColor
+import androidx.compose.ui.unit.dp
+import team.retum.savage_android.ui.theme.SavageTypography
 
 @Composable
-internal fun SavageButton(
-    modifier: Modifier = Modifier,
-    color: Color = SavageColor.Gray100,
-    enabled: Boolean = true,
-    text: String,
+fun SavageButton(
+    modifier: Modifier = Modifier
+        .fillMaxWidth()
+        .background(Color.Transparent)
+        .padding(horizontal = 4.dp),
+    text: String = "default",
     onClick: () -> Unit,
+    isAbleClick: Boolean = false,
 ) {
-    Surface(
-        modifier = modifier
-            .fillMaxWidth()
-            .background(color),
-        enabled = enabled,
-        onClick = onClick,
-    ){
+    var isPressed by remember { mutableStateOf(false) }
 
+    val colors = ButtonDefaults.buttonColors(
+        containerColor = when {
+            isAbleClick -> if (isPressed) Color(0xFF1E864A) else Color(0xFF27AF62)
+            else -> if (isPressed) Color(0xFF1E864A) else Color(0xFFB4EECD)
+        },
+        contentColor = Color.White,
+        disabledContainerColor = Color(0xFFB4EECD),
+        disabledContentColor = Color(0xFFFFFFFF),
+    )
+
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressedState by interactionSource.collectIsPressedAsState()
+
+    LaunchedEffect(isPressedState) {
+        isPressed = isPressedState
     }
+
+    Button(
+        onClick = {
+            onClick()
+        },
+        enabled = isAbleClick,
+        interactionSource = interactionSource,
+        shape = RoundedCornerShape(4.dp),
+        colors = colors,
+        modifier = modifier
+    ) {
+        Text(text = text, style = SavageTypography.Body2)
+    }
+}
+
+@Preview
+@Composable
+fun SavageButtonPreview() {
+    SavageButton(
+        text = "Savage Button",
+        onClick = { println("Savage Button Clicked") },
+        isAbleClick = false
+    )
 }
